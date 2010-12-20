90c313388af5a74c489bb4eb60509f35e001914e
Radosław Palczyński <radepal@o2.pl>
2010-12-20 11:21:00 +0100

    change links extracted before page_block

--- lib/anemone/page_worker.rb
+++ lib/anemone/page_worker.rb
@@ -29,12 +29,14 @@ module Anemone
 
         @pages.touch_key page.url
         puts "#{page.url} Queue link: #{@link_queue.size} Queue page: #{@page_queue.size}" if @opts[:verbose]
-        do_page_blocks page
+       
         page.discard_doc! if @opts[:discard_page_bodies]
         links = links_to_follow page
+         do_page_blocks page
         links.each do |link|
           @link_queue << [link, page.url.dup, page.depth + 1]
         end
+
         @pages.touch_keys links
 
         @pages[page.url] = page
